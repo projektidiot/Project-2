543f349c122d3b57546dfa989723fe1288da0a53
Jennifer Rice <dnjen@live.com>
2012-03-01 01:10:40 -0500

    edits

--- candidates2.php
+++ candidates2.php
@@ -11,75 +11,76 @@
 
     <link rel="stylesheet" type="text/css" href="css/styles.css" />
 
-<? 
-	if(isset('submit')
+	<? include("db_connect.php"); ?>
+	<? include("vote_counter.php"); ?>
 
-?>
 </head>
 <body>
 
 <div class="container">
-	 <form method="POST" action="candidates2.php">  
+	 <form method="POST" action="hasvoted.php">
 	
-	<div id="page1" data-role="page">
-		<div>
-			<h1>Toon Town Election</h1>
-			<h3>Vote for your favorite candidate today!</h3>
-		</div>
-	
-		<div id="candidate1">
-			<div class="transbox">
-				<h4>Calvin</h4>
-				<img src="img/calvin.jpg" alt="Calvin of Calvin and Hobbs" />
-				<p>Don&rsquo;t be fooled by his innocent manner, he knows what he&rsquo;s doing.</p>
-				<input name="submit" type="submit" id="submit" value="VOTE!" />
-				<a href="#page2">To Page 2</a> 
+		<div id="page1" data-role="page">
+			<div>
+				<h1>Toon Town Election</h1>
+				<h3>Vote for your favorite candidate today!</h3>
 			</div>
-		</div> 
-
-			
-	</div> <!-- End Page 1 -->
+		
+			<div id="candidate1">
+				<div class="transbox">
+					<h4>Calvin</h4>
+					<img src="img/calvin.jpg" alt="Calvin of Calvin and Hobbs" />
+					<p>Don&rsquo;t be fooled by his innocent manner, he knows what he&rsquo;s doing.</p>
+					<input name="submit_Calvin" type="submit" id="Calvin" value="Vote for Calvin" />
+					<a href="#page2">To Page 2</a> 
+				</div>
+			</div> 
 	
-
-	<div id="page2" data-role="page">
-		<div>
-			<h1>Toon Town Election</h1>
-			<h3>Vote for your favorite candidate today!</h3>
-		</div>
+				
+		</div> <!-- End Page 1 -->
+		
 	
-		<div id="candidate2">
-			<div class="transbox">
-				<h4>Sonic the Hedgehog</h4>
-				<img src="img/sonic.jpg" alt="Sonic the Hedgehog" />
-				<p>He&rsquo;s running for freedom, and running for all!</p>
-				<input name="submit" type="submit" id="submit" value="VOTE!" />
-				<a href="#page3">To Page 3</a>
+		<div id="page2" data-role="page">
+			<div>
+				<h1>Toon Town Election</h1>
+				<h3>Vote for your favorite candidate today!</h3>
 			</div>
-		</div>
-			
-	</div> <!-- End Page 2 -->
-	
-
-	<div id="page3" data-role="page">
-		<div>
-			<h1>Toon Town Election</h1>
-			<h3>Vote for your favorite candidate today!</h3>
-		</div>
-	
-		<div id="candidate3">
-			<div class="transbox">
-				<h4>Princess Peach</h4>
-				<img src="img/peach.jpg" alt="Princess Peach" />
-				<p>She will clear out the pipes of bureaucracy with her infallible justice.</p>
-				<input name="submit" type="submit" id="submit" value="VOTE!" />
-				<a href="#page1">To Page 1</a>
+		
+			<div id="candidate2">
+				<div class="transbox">
+					<h4>Sonic the Hedgehog</h4>
+					<img src="img/sonic.jpg" alt="Sonic the Hedgehog" />
+					<p>He&rsquo;s running for freedom, and running for all!</p>
+					<input name="submit_Sonic" type="submit" id="Sonic" value="Vote for Sonic" />
+					<a href="#page3">To Page 3</a>
+				</div>
 			</div>
-		</div>
-
-			
-	</div> <!-- End Page 3 -->
-
-	</div>
+				
+		</div> <!-- End Page 2 -->
+		
+	
+		<div id="page3" data-role="page">
+			<div>
+				<h1>Toon Town Election</h1>
+				<h3>Vote for your favorite candidate today!</h3>
+			</div>
+		
+			<div id="candidate3">
+				<div class="transbox">
+					
+					<h4>Princess Peach</h4>
+					<img src="img/peach.jpg" alt="Princess Peach" />
+					<p>She will clear out the pipes of bureaucracy with her infallible justice.</p>
+					<input name="submit_Peach" type="submit" id="Princess Peach" value="Vote for Princess Peach" />
+					<a href="#page1">To Page 1</a>
+				</div>
+			</div>
+	
+				
+		</div> <!-- End Page 3 -->
+		
+	</form>
 </div>
 </body>
 </html>
+<? $mysqli->close(); ?> 
