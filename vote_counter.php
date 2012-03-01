c736da42781c4554ef013721ea12ed6c3f75da30
Jennifer Rice <dnjen@live.com>
2012-03-01 01:33:11 -0500

    vote for me

--- vote_counter.php
+++ vote_counter.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$query = "SELECT * FROM candidate_table";
+$query = "SELECT * FROM candidates_table";
 
 $result = $mysqli->query($query); 
 if($mysqli->error) {
@@ -15,12 +15,10 @@ while($row = $result->fetch_object()) {
 	if ($row->candidate_name == "Calvin") {
 		$numberofVotes_calvin = $row->num_of_votes;
 	}
-	for(){
-	if ($row->candidate_name == "Sonic the Hedgehog") {
+	else if ($row->candidate_name == "Sonic the Hedgehog") {
 		$numberofVotes_sonic = $row->num_of_votes;
 	}
-	for(){
-	if ($row->candidate_name == "Princess Peach") {
+	else if ($row->candidate_name == "Princess Peach") {
 		$numberofVotes_peach = $row->num_of_votes;
 	}
 
@@ -28,25 +26,25 @@ while($row = $result->fetch_object()) {
 
 
 
-if(isset($_POST['submit_calvin'] == "Vote for Calvin") {
-$numberofVotes_calvin += 1;
-	insert_query = "INSERT '".."'";
+if ( isset($_POST['submit_calvin']) && ($_POST['submit_calvin'] == "Vote for Calvin") ) {
+	$numberofVotes_calvin .= 1;
+	$insert_query = "INSERT into num_of_votes('".$numberofVotes_calvin."')";
 	header('Location: hasvoted.php');
 
-}
+} /*
 else if(isset($_POST['submit_sonic'] == "Vote for Sonic") {
-$numberofVotes_sonic += 1;
-	insert_query = "";
+	$numberofVotes_sonic .= 1;
+	$insert_query = "INSERT into num_of_votes('".$numberofVotes_sonic."')";
 	header('Location: hasvoted.php');
 
 }
 else if(isset($_POST['submit_peach'] == "Vote for Princess Peach") {
-$numberofVotes_peach += 1;
-	insert_query = "";
+	$numberofVotes_peach .= 1;
+	$insert_query = "INSERT into num_of_votes('".$numberofVotes_peach."')";
 	header('Location: hasvoted.php');
 
 }
-
+*/
 
 
 
